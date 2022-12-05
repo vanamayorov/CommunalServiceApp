30f55ff799e5c8f7ab18a5f7d40a8a7c408d07fb
Ivan <denatarelcom2105@gmail.com>
2022-12-05 19:02:02 +0200

    finished changeAddress modal

--- force-app/main/default/lwc/changeAddressModal/changeAddressModal.js
+++ force-app/main/default/lwc/changeAddressModal/changeAddressModal.js
@@ -1,4 +1,5 @@
 import LightningModal from "lightning/modal";
+import PaymentModal from "c/paymentModal";
 import { wire } from "lwc";
 import {
   subscribe,
@@ -7,12 +8,22 @@ import {
 } from "lightning/messageService";
 import { refreshApex } from "@salesforce/apex";
 import userHasDebt from "@salesforce/apex/CommunalServiceUserController.userHasDebt";
-import PaymentModal from "c/paymentModal";
+import getRegions from "@salesforce/apex/RegionController.getRegions";
+import changeAddress from "@salesforce/apex/CommunalServiceUserController.changeAddress";
+import getCommunalServiceUserRegion from "@salesforce/apex/CommunalServiceUserController.getCommunalServiceUserRegion";
 import UPDATE_AFTER_PAYMENT_CHANNEL from "@salesforce/messageChannel/Update_After_Payment__c";
 import Id from "@salesforce/user/Id";
 
 export default class ChangeAddressModal extends LightningModal {
-  region = null;
+  regionsData = [];
+  regionsOptions = [];
+  cities = [];
+  regionId = "";
+  cityId = "";
+  managerId = "";
+  currentRegion = "";
+  currentCity = "";
+  successfullyChangedAdress = false;
   userHasDebt = false;
   debtsResult = null;
   subscription = null;
@@ -20,6 +31,21 @@ export default class ChangeAddressModal extends LightningModal {
 
   connectedCallback() {
     this.subscribeToMessageChannel();
+    this.fetchCurrentAddress();
+  }
+
+  @wire(getRegions)
+  wiredRegions({ data, error }) {
+    if (data) {
+      this.regionsData = data;
+      this.regionsOptions = this.regionsData.map((reg) => ({
+        label: reg.Name,
+        value: reg.Id
+      }));
+    }
+    if (error) {
+      this.error = error;
+    }
   }
 
   @wire(userHasDebt, { userId: Id })
@@ -47,11 +73,75 @@ export default class ChangeAddressModal extends LightningModal {
     );
   }
 
+  fetchCurrentAddress() {
+    getCommunalServiceUserRegion({ userId: Id })
+      .then((res) => {
+        this.currentRegion = res.Region__r.Name;
+        this.currentCity = res.City__r.Name;
+      })
+      .catch((err) => {
+        this.error = err;
+      });
+  }
+
+  handleChangeRegion(e) {
+    this.regionId = e.target.value;
+    this.cityId = "";
+    this.error = null;
+
+    const region = this.regionsData.find((reg) => reg.Id === this.regionId);
+    if (region?.Region_Managers__r) {
+      this.managerId = region.Region_Managers__r[0].Id;
+    } else {
+      this.error = new Error(
+        "This region has no manager, please select another one"
+      );
+    }
+
+    this.cities =
+      this.regionsData
+        .find((regions) => regions.Id === this.regionId)
+        ?.Cities__r?.filter((city) => city.Name !== this.currentCity)
+        .map((city) => ({
+          label: city.Name,
+          value: city.Id
+        })) ?? [];
+  }
+
+  handleChangeCity(e) {
+    this.cityId = e.target.value;
+  }
+
+  handleChangeAddress() {
+    const payload = {
+      regionId: this.regionId,
+      cityId: this.cityId,
+      userId: Id,
+      managerId: this.managerId
+    };
+
+    changeAddress({ payload: JSON.stringify(payload) })
+      .then(() => {
+        this.successfullyChangedAdress = true;
+      })
+      .catch((err) => {
+        this.error = err;
+      });
+  }
+
   handlePayBills() {
     PaymentModal.open({ size: "small" });
   }
 
+  handleClose() {
+    this.close();
+  }
+
   disconnectedCallback() {
     unsubscribe(this.subscription);
+  }
+
+  get changeAddressBtnDisabled() {
+    return !(this.regionId && this.cityId && !this.error);
   }
 }
